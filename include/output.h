de4b527fd8da8c9130c37c94cecd354872a0cc54
Francesco Andreucci <fra.andreuc@gmail.com>
2023-11-24 09:34:51 +0100

    Francesco: added output.c and output.h

---
+++ include/output.h
@@ -0,0 +1,16 @@
+#ifndef OUTPUT_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define OUTPUT_H
+
+#include"types.h"
+#include<stdio.h>
+
+void output(mdsys_t *sys, FILE *erg, FILE *traj);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
