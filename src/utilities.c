ba804194aa6a0bd37b1595d13b4ea1cf5119d790
MrZack-HPC <zdahbi@sissa.it>
2023-11-24 09:52:18 +0100

    zakaria: added utilities.c and utilities.h

---
+++ src/utilities.c
@@ -0,0 +1,40 @@
+#include"types.h"
+#include"constants.h"
+#include <sys/time.h>
+
+/* helper function: get current time in seconds since epoch */
+double wallclock()
+{
+        struct timeval t;
+        gettimeofday(&t,0);
+        return ((double) t.tv_sec) + 1.0e-6*((double) t.tv_usec);
+}
+
+/* helper function: apply minimum image convention */
+double pbc(double x, const double boxby2)
+{
+    while (x >  boxby2) x -= 2.0*boxby2;
+    while (x < -boxby2) x += 2.0*boxby2;
+    return x;
+}
+
+/* compute kinetic energy */
+void ekin(mdsys_t *sys)
+{
+    int i;
+
+    sys->ekin=0.0;
+    for (i=0; i<sys->natoms; ++i) {
+        sys->ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
+    }
+    sys->temp = 2.0*sys->ekin/(3.0*sys->natoms-3.0)/kboltz;
+}
+
+/* helper function: zero out an array */
+void azzero(double *d, const int n)
+{
+    int i;
+    for (i=0; i<n; ++i) {
+        d[i]=0.0;
+    }
+}
