fd70e7e8a4f35b141dbd63a75b78c2ade47b14a1
Francesco Andreucci <fra.andreuc@gmail.com>
2023-11-27 15:11:56 +0100

    Francesco: changed main.cpp so that it can run with mpi

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@
 #include <stdlib.h>
 
 
+#ifdef _MPI
+#include "mpi.h"
+#endif
+
 #define LJMD_VERSION 0.1
 
 
@@ -20,27 +24,39 @@ int main(int argc, char **argv){
     FILE *fp,*traj,*erg;
     mdsys_t sys;
     double t_start;
+    sys.nsize=1;
+    sys.mpirank=0;
+#ifdef _MPI
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &sys.nsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &sys.mpirank);
+#endif
 
+    if(sys.mpirank==0){
     printf("LJMD version %3.1f\n", LJMD_VERSION);
 
     t_start = wallclock();
 
     /* read input file */
     read_from_file( &sys, &nprint,restfile,trajfile,ergfile,line);
-
-
+    } //endif myrank=0
+    sys.nfi=0;
+    //brodcasts
+    //reset of rank
     /* allocate memory */
     memalloc(&sys);
-
+if(sys.myrank==0){
     /* read restart */
     read_restfile(restfile, &sys);
-
+}// endif myrank=0
 
     /* initialize forces and energies.*/
-    sys.nfi=0;
+
     force(&sys);
+
     ekin(&sys);
 
+if(sys.myrank==0){
     erg=fopen(ergfile,"w");
     traj=fopen(trajfile,"w");
 
@@ -51,9 +67,12 @@ int main(int argc, char **argv){
 
     /* reset timer */
     t_start = wallclock();
+}
+
 
     /**************************************************/
     /* main MD loop */
+
     for(sys.nfi=1; sys.nfi <= sys.nsteps; ++sys.nfi) {
 
         /* write output, if requested */
@@ -72,7 +91,11 @@ int main(int argc, char **argv){
     /* clean up: close files, free memory */
     printf("Simulation Done. Run time: %10.3fs\n", wallclock()-t_start);
 
+
     cleanup(erg,traj,sys);
 
+#ifdef _MPI
+    MPI_Finalize();
+#endif
     return 0;
 }
