f5b82e0132aacda8e6d252701d3f9801c187f06c
Francesco Andreucci <fra.andreuc@gmail.com>
2023-11-29 11:38:08 +0100

    francesco: modified test_forces to run also with mpi

--- tests/test_forces.cpp
+++ tests/test_forces.cpp
@@ -3,6 +3,11 @@
 #include "force_comp.h"
 #include "utilities.h"
 
+#ifdef LJMD_MPI
+#include "mpi.h"
+#endif
+
+
 class ForceTest: public ::testing::Test {
 
 protected:
@@ -19,6 +24,8 @@ protected:
         sys->box=17.158000;
         sys->dt = 5.0;
         sys->mass = 1.0 ;
+        sys->mpirank=0;
+        sys->nsize=1;
         sys->rx = new double[2];
         sys->vx = new double[2];
         sys->fx = new double[2];
@@ -28,6 +35,11 @@ protected:
         sys->rz = new double[2];
         sys->vz = new double[2];
         sys->fz = new double[2];
+#ifdef LJMD_MPI
+        sys->cx = new double[2];
+        sys->cy = new double[2];
+        sys->cz = new double[2];
+#endif
         sys->rx[0] = 0.0;
         sys->rx[1] = 0.0;
         sys->ry[0] = 0.0;
@@ -46,6 +58,14 @@ protected:
         sys->fy[1] = 0.0;
         sys->fz[0] = 0.0;
         sys->fz[1] = 0.0;
+#ifdef LJMD_MPI
+        sys->cx[0] = 0.0;
+        sys->cx[1] = 0.0;
+        sys->cy[0] = 0.0;
+        sys->cy[1] = 0.0;
+        sys->cz[0] = 0.0;
+        sys->cz[1] = 0.0;
+#endif
 
     }
 
@@ -60,6 +80,11 @@ protected:
             delete[] sys->rz;
             delete[] sys->vz;
             delete[] sys->fz;
+#ifdef LJMD_MPI
+            delete[] sys->cx;
+            delete[] sys->cz;
+            delete[] sys->cy;
+#endif
             delete sys;
         }
 };
@@ -67,13 +92,17 @@ protected:
 TEST_F(ForceTest, force0)
 {
 //The particles are further apart than rcut, but inside the box: the force should be zero
+#ifdef LJMD_MPI
+    MPI_Comm_rank(MPI_COMM_WORLD, &sys->mpirank);
+    MPI_Comm_size(MPI_COMM_WORLD, &sys->nsize);
+#endif
     sys->rx[0]=1.0;
     sys->rx[1]=1.1+sys->rcut;
     sys->ry[0]=1.0;
     sys->ry[1]=1.1+sys->rcut;
     sys->rz[0]=1.0;
     sys->rz[1]=1.1+sys->rcut;
-
+if(sys->mpirank==0){
     ASSERT_NE(sys,nullptr);
     ASSERT_DOUBLE_EQ(sys->rx[0], 1.0);
     ASSERT_DOUBLE_EQ(sys->rx[1],1.1+sys->rcut);
@@ -81,7 +110,9 @@ TEST_F(ForceTest, force0)
     ASSERT_DOUBLE_EQ(sys->ry[1],1.1+sys->rcut);
     ASSERT_DOUBLE_EQ(sys->rz[0], 1.0);
     ASSERT_DOUBLE_EQ(sys->rz[1],1.1+sys->rcut);
+}
     force(sys);
+if(sys->mpirank==0){
     ASSERT_DOUBLE_EQ(sys->fx[0],0.0);
     ASSERT_DOUBLE_EQ(sys->fx[1],0.0);
     ASSERT_DOUBLE_EQ(sys->fy[0],0.0);
@@ -89,20 +120,24 @@ TEST_F(ForceTest, force0)
     ASSERT_DOUBLE_EQ(sys->fz[0],0.0);
     ASSERT_DOUBLE_EQ(sys->fz[1],0.0);
 }
+}
 
 
 TEST_F(ForceTest, forceno0)
 {
 /*In this case the particles are close enough to interact: we compare the
  * output of the force command with reference values.
- */
+ */#ifdef LJMD_MPI
+    MPI_Comm_rank(MPI_COMM_WORLD, &sys->mpirank);
+    MPI_Comm_size(MPI_COMM_WORLD, &sys->nsize);
+   #endif
     sys->rx[0]=1.0;
     sys->rx[1]=2.1;
     sys->ry[0]=1.0;
     sys->ry[1]=2.2;
     sys->rz[0]=1.0;
     sys->rz[1]=2.3;
-
+if(sys->mpirank==0){
     ASSERT_NE(sys,nullptr);
     ASSERT_DOUBLE_EQ(sys->rx[0], 1.0);
     ASSERT_DOUBLE_EQ(sys->rx[1],2.1);
@@ -110,7 +145,9 @@ TEST_F(ForceTest, forceno0)
     ASSERT_DOUBLE_EQ(sys->ry[1],2.2);
     ASSERT_DOUBLE_EQ(sys->rz[0], 1.0);
     ASSERT_DOUBLE_EQ(sys->rz[1],2.3);
+}
     force(sys);
+if(sys->mpirank==0){
     ASSERT_DOUBLE_EQ(sys->fx[0],-1024.386173537659715);
     ASSERT_DOUBLE_EQ(sys->fx[1],1024.386173537659715);
     ASSERT_DOUBLE_EQ(sys->fy[0],-1117.512189313810723);
@@ -118,19 +155,23 @@ TEST_F(ForceTest, forceno0)
     ASSERT_DOUBLE_EQ(sys->fz[0],-1210.638205089961275);
     ASSERT_DOUBLE_EQ(sys->fz[1],1210.638205089961275);
 }
+}
 
 
 TEST_F(ForceTest, forceno0pbc)
 {
 /*In this case the particles lie on the same yz plane: the interaction is only along the x direction, but the interparticle distance is nominally larger than box edge.
- */
+ */ #ifdef LJMD_MPI
+    MPI_Comm_rank(MPI_COMM_WORLD, &sys->mpirank);
+    MPI_Comm_size(MPI_COMM_WORLD, &sys->nsize);
+   #endif
     sys->rx[0]=3.7;
     sys->rx[1]=23.558;
     sys->ry[0]=4.6;
     sys->ry[1]=4.6;
     sys->rz[0]=1.9;
     sys->rz[1]=1.9;
-
+if(sys->mpirank==0){
     ASSERT_NE(sys,nullptr);
     ASSERT_DOUBLE_EQ(sys->rx[0], 3.7);
     ASSERT_DOUBLE_EQ(sys->rx[1],23.558);
@@ -138,7 +179,9 @@ TEST_F(ForceTest, forceno0pbc)
     ASSERT_DOUBLE_EQ(sys->ry[1],4.6);
     ASSERT_DOUBLE_EQ(sys->rz[0], 1.9);
     ASSERT_DOUBLE_EQ(sys->rz[1],1.9);
+}
     force(sys);
+if(sys->mpirank==0){
     ASSERT_DOUBLE_EQ(sys->fx[0],-59.933619233522784);
     ASSERT_DOUBLE_EQ(sys->fx[1],59.933619233522784);
     ASSERT_DOUBLE_EQ(sys->fy[0],0.0);
@@ -146,7 +189,32 @@ TEST_F(ForceTest, forceno0pbc)
     ASSERT_DOUBLE_EQ(sys->fz[0],0.0);
     ASSERT_DOUBLE_EQ(sys->fz[1],0.0);
 }
+}
+
+int main(int argc, char** argv) {
+
+    ::testing::InitGoogleTest(&argc, argv);
+#ifdef LJMD_MPI
+    MPI_Init(&argc, &argv);
+#endif
+    int rank, size;
+    #ifdef LJMD_MPI
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+   #endif
 
 
+    int result = RUN_ALL_TESTS();
+    printf("%d\n", result);
+
+
+
+
+#ifdef LJMD_MPI
+    MPI_Finalize();
+#endif
+
+    return 0;
+}
 
 
