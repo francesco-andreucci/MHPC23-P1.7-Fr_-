1924a89fefc57010f0842a99b4ba9cce85239939
MrZack-HPC <zdahbi@sissa.it>
2023-11-29 13:20:33 +0100

    zakaria: get ride of redundant things and make the code similar to mpi one for the ease of merging

--- src/force_comp.c
+++ src/force_comp.c
@@ -8,29 +8,24 @@
 
 void force(mdsys_t *sys) {
     
-    double epot = 0.00000;
-    double c12,c6,rcsq;
-    int j, tid, start, end;
-    double pow6, pow12;
-
-    /* defining pow by repeating product */
-    pow6 = sys->sigma * sys->sigma * sys->sigma * sys->sigma * sys->sigma * sys->sigma;
-    pow12 = sys->sigma * sys->sigma * sys->sigma * sys->sigma * sys->sigma * sys->sigma *\
-                sys->sigma * sys->sigma * sys->sigma * sys->sigma * sys->sigma * sys->sigma;
+    int tid, start, end;
+    // double rx1, ry1, rz1;
+    int i, j;
+    double rx, ry, rz, rsq;
+    double r6, rinv, ffac;
 
     /* zero energy and forces */
-    c12 = 4.000 * sys->epsilon * pow12; // pow(sys->sigma, 12.000);
-    c6 = 4.000 * sys->epsilon * pow6;      // pow(sys->sigma, 6.000);
-    rcsq = sys->rcut * sys->rcut;
+    double c12 = 4.0 * sys->epsilon * pow(sys->sigma, 12.0);
+    double c6 = 4.0 * sys->epsilon * pow(sys->sigma, 6.0);
+    double rcsq = sys->rcut * sys->rcut;
+
+    double epot = 0.0;
 
     #ifdef _OPENMP
-        #pragma omp parallel reduction(+:epot) firstprivate(c12, c6, rcsq, j, tid, start, end, pow6, pow12)
+        #pragma omp parallel reduction(+:epot) firstprivate(c12, c6, rcsq, i, j, tid, start, end, rx, ry, rz, rsq, r6, rinv, ffac)
     #endif
     { 
         double *fx, *fy, *fz;
-        double rx1, ry1, rz1;
-        double rx, ry, rz, rsq;
-        double r6, rinv, ffac;
   
         /* reading thread id and defining default tid when omp is absent */
         #ifdef _OPENMP
@@ -47,20 +42,16 @@ void force(mdsys_t *sys) {
         azzero(fy, sys->natoms);
         azzero(fz, sys->natoms);
 
-        for (int i = 0; i < (sys->natoms - 1); i += sys->tmax)
+        for (i = 0; i < (sys->natoms - 1); i += sys->tmax)
         {
             int ii = i + tid;
-            if (ii >= (sys->natoms - 1)) break;
-
-            rx1 = sys->rx[ii];
-            ry1 = sys->ry[ii];
-            rz1 = sys->rz[ii];
+            if(ii >= (sys->natoms - 1)) break;
 
             for (j = ii + 1; j < sys->natoms; ++j)
             {
-                rx = pbc(rx1 - sys->rx[j], 0.500 * sys->box);
-                ry = pbc(ry1 - sys->ry[j], 0.500 * sys->box);
-                rz = pbc(rz1 - sys->rz[j], 0.500 * sys->box);
+                rx = pbc(sys->rx[ii] - sys->rx[j], 0.500 * sys->box);
+                ry = pbc(sys->ry[ii] - sys->ry[j], 0.500 * sys->box);
+                rz = pbc(sys->rz[ii] - sys->rz[j], 0.500 * sys->box);
                 rsq = (rx * rx) + (ry * ry) + (rz * rz);
                     
                 if (rsq < rcsq)
@@ -92,7 +83,7 @@ void force(mdsys_t *sys) {
             end = sys->natoms; 
         }
 
-        for(int i = 1; i < sys->tmax; ++i)
+        for(i = 1; i < sys->tmax; ++i)
         {   
             int offset = i * sys->natoms;
             for(int j = start; j < end; ++j)
